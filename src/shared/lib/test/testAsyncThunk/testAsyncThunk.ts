db48fac1e06754d7da62aeaf6bec02dd1e36e1c6
Eremor <artem.s13@gmail.com>
2023-11-23 15:25:26 +0300

    feat: add lib for testing async thunk

---
+++ src/shared/lib/test/testAsyncThunk/testAsyncThunk.ts
@@ -0,0 +1,49 @@
+import axios, { AxiosStatic } from 'axios';
+import { AsyncThunkAction } from "@reduxjs/toolkit";
+import { StateSchema } from "app/providers/StoreProvider";
+
+type ActionCreatorType<Return, Arg, RejectedValue> = (arg: Arg) => AsyncThunkAction<Return, Arg, { rejectValue: RejectedValue }>;
+
+jest.mock('axios');
+
+const mockedAxios = jest.mocked(axios);
+
+export class TestAsyncThunk<Return, Arg, RejectedValue> {
+  dispatch: jest.MockedFn<any>;
+
+  getState: () => StateSchema;
+
+  actionCreator: ActionCreatorType<Return, Arg, RejectedValue>;
+
+  api: jest.MockedFunctionDeep<AxiosStatic>;
+
+  socket: jest.MockedFn<any>;
+
+  navigate: jest.MockedFn<any>;
+
+  constructor(actionCreator: ActionCreatorType<Return, Arg, RejectedValue>) {
+    this.dispatch = jest.fn();
+    this.getState = jest.fn();
+    this.actionCreator = actionCreator;
+
+    this.api = mockedAxios;
+    this.socket = jest.fn();
+    this.navigate = jest.fn();
+  }
+
+  async callThunk(arg: Arg) {
+    const action = this.actionCreator(arg);
+
+    const result = await action(
+      this.dispatch,
+      this.getState,
+      {
+        api: this.api,
+        socket: this.socket,
+        navigate: this.navigate
+      }
+    );
+
+    return result;
+  }
+}
